b1e5aa90e6f7211366a6fd5c73c0a27b069e6b6f
Vikram Singh <vs125609@gmail.com>
2023-11-09 13:52:36 +0530

    render the list of users

--- routes/profile.js
+++ routes/profile.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const profiles = [
   {
     "id": 1,
-    "name": "A Martinez",
+    "name": "Vikram Singh",
     "description": "Adolph Larrue Martinez III.",
     "mbti": "ISFJ",
     "enneagram": "9w3",
@@ -15,15 +15,54 @@ const profiles = [
     "socionics": "SEE",
     "sloan": "RCOEN",
     "psyche": "FEVL",
-    "image": "https://soulverse.boo.world/images/1.png",
-  }
+    "image": "https://avatars.githubusercontent.com/u/93823479?v=4",
+  },
+  {
+    "id": 2,
+    "name": "Giridhar Gopal",
+    "description": "Adolph Larrue Martinez III.",
+    "mbti": "ISFJ",
+    "enneagram": "9w3",
+    "variant": "sp/so",
+    "tritype": 725,
+    "socionics": "SEE",
+    "sloan": "RCOEN",
+    "psyche": "FEVL",
+    "image": "https://avatars.githubusercontent.com/u/93823479?v=4",
+  },
+  {
+    "id": 3,
+    "name": "Premanand Ji Maharaj",
+    "description": "Adolph Larrue Martinez III.",
+    "mbti": "ISFJ",
+    "enneagram": "9w3",
+    "variant": "sp/so",
+    "tritype": 725,
+    "socionics": "SEE",
+    "sloan": "RCOEN",
+    "psyche": "FEVL",
+    "image": "https://avatars.githubusercontent.com/u/93823479?v=4",
+  },
+  {
+    "id": 4,
+    "name": "Sadhvi Devavani",
+    "description": "Adolph Larrue Martinez III.",
+    "mbti": "ISFJ",
+    "enneagram": "9w3",
+    "variant": "sp/so",
+    "tritype": 725,
+    "socionics": "SEE",
+    "sloan": "RCOEN",
+    "psyche": "FEVL",
+    "image": "https://avatars.githubusercontent.com/u/93823479?v=4",
+  },
 ];
 
-module.exports = function() {
+module.exports = function () {
 
   router.get('/*', function(req, res, next) {
     res.render('profile_template', {
-      profile: profiles[0],
+      profile: profiles,
     });
   });
 
